274a06a3c2a5cfbe07aae70c614d8db7b89a5bd5
stjason022 <stjason022@student.wethinkcode.co.za>
2023-03-13 15:22:03 +0200

    working on tests

---
+++ core/src/com/game/BoardUtils.java
@@ -0,0 +1,24 @@
+package com.game;
+
+import com.badlogic.gdx.utils.Array;
+
+public abstract class BoardUtils {
+
+
+    public static BoardSpace getPreviouslyTouchedSpace(Array<Array<BoardSpace>> boardSpaces){
+        for(byte rowIndex = 0; rowIndex < 8; rowIndex++){
+            Array<BoardSpace> boardRow = boardSpaces.get(rowIndex);
+
+            for (byte columnIndex = 0; columnIndex < 8; columnIndex++){
+                BoardSpace boardSpace = boardRow.get(columnIndex);
+
+                if(boardSpace.isSelected() == 1){
+                    return boardSpace;
+                }
+
+            }
+        }
+        return null;
+    }
+
+}
